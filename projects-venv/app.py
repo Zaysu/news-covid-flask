d15e7a15348a84262f26b99694cd95362fe4cc38
Zaysu <80471248+Zaysu@users.noreply.github.com>
2022-03-08 14:16:48 -0300

    2.0
    atualização do commit e appy.py

--- projects-venv/app.py
+++ projects-venv/app.py
@@ -2,13 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 
+
 @app.route("/hello")
 def ola_mundo():
     return "Ola Mundo"
 
+
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route("/news")
+def news():
+    return render_template('news.html')
+
+
+@app.route("/nl")
+def nl():
+    return render_template('newsletter.html')
+
 
 if __name__ == "__main__":
     app.run()
